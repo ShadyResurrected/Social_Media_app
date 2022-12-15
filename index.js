ce8a1d2a639199c2e6dfe5978bcab4062301950f
ShadyResurrected <cchandra554@gmail.com>
2022-12-15 11:31:57 +0530

    Storing images of posted pics in public folder

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const helmet = require("helmet")
-const morgan = require("morgan")
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
-var cors = require('cors')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+var cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 
-dotenv.config()
+dotenv.config();
 
-app.use(cors())
+app.use(cors());
 
-mongoose.set('strictQuery', true); // To resolve the warning of mongo version 7
+mongoose.set("strictQuery", true); // To resolve the warning of mongo version 7
 
-mongoose.connect(process.env.MONGO_URL,() => {
-    console.log("Connected to MongoDB")
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connected to MongoDB");
 });
 
+// When making '{/images}' this path request redirect to '{public/images}'
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // Middleware
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("common"))
+app.use(express.json());
+// To resolve cors issue of uploading image
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+app.use(morgan("common"));
 
-app.use("/api/users", userRoute)
-app.use("/api/auth", authRoute)
-app.use("/api/posts", postRoute)
+// Used to store the images of shared posts in public folder
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images"); // Error here solve
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
+const upload = multer({ storage : storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File Uploaded succesfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.listen(8800,() => {
-    console.log("Server running")
-})
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Server running");
+});
